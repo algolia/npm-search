8b919332fa2ce464d3901abc5bb6d6eb0b63c3a0
vvo <vincent.voyer@algolia.com>
2016-12-19 14:43:44 +0100

    handle bad package content at the right level
    Do not try to return a formatted package if not valid

--- saveChanges.js
+++ saveChanges.js
@@ -7,7 +7,7 @@ export default function saveChangesAndState(seq, changes) {
   const rawPkgs = changes
     .filter(result => result.doc.name !== undefined) // must be a document
     .map(result => formatPkg(result.doc))
-    .filter(pkg => pkg !== undefined && pkg.objectID); // we're using the name as an objectID, might be blank
+    .filter(pkg => pkg !== undefined);
 
   if (rawPkgs.length === 0) {
     log.info('No pkgs found in changes.');
