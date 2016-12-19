80f37910bdd6fd25881d09034694400b5b96fb76
Sylvain UTARD <sylvain.utard@gmail.com>
2016-12-19 09:12:09 +0100

    Add index settings

--- algoliaIndex.js
+++ algoliaIndex.js
@@ -4,4 +4,14 @@ import c from './config.js';
 if (!c.apiKey) throw new Error('npm-search: Please provide the `apiKey` env variable and restart');
 
 const client = algoliasearch(c.appId, c.apiKey);
-export default client.initIndex(c.indexName);
+const index = client.initIndex(c.indexName);
+
+index.setSettings({
+  searchableAttributes: ['unordered(name)', 'unordered(description)',
+    'unordered(keywords)', 'author.name', 'owners.name'],
+  customRanking: ['desc(downloadsLast30Days)'],
+  exactOnSingleWordQuery: 'word',
+  ranking: ['asc(popular)', 'typo', 'geo', 'words', 'filters', 'proximity', 'attribute', 'exact', 'custom'],
+});
+
+export default index;
