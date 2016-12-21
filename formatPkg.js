5249abbd56784edd6de11d5826941adb49cd7963
vvo <vincent.voyer@algolia.com>
2016-12-21 15:37:25 +0100

    add more precomputed data

--- formatPkg.js
+++ formatPkg.js
@@ -1,59 +1,66 @@
 import NicePackage from 'nice-package';
 import gravatarUrl from 'gravatar-url';
 import numeral from 'numeral';
+const defaultGravatar = 'https://www.gravatar.com/avatar/?size=180';
+import escape from 'escape-html';
 
 export default function formatPkg(pkg) {
-  const formatted = new NicePackage(pkg);
+  const cleaned = new NicePackage(pkg);
 
-  if (formatted.valid === false) {
+  if (cleaned.valid === false || cleaned.lastPublisher === undefined) {
     return undefined;
   }
 
+  const githubRepo = getGitHubRepoInfo(cleaned.repository);
+  const lastPublisher = formatUser(cleaned.lastPublisher);
+  const owner = getOwner(githubRepo, lastPublisher);
+  let keywords = [];
+
+  if (cleaned.keywords) {
+    if (Array.isArray(cleaned.keywords)) keywords = [...cleaned.keywords];
+    if (typeof cleaned.keywords === 'string') keywords = [cleaned.keywords];
+  }
+
   return {
-    objectID: formatted.name,
-    name: formatted.name,
+    objectID: cleaned.name,
+    name: escape(cleaned.name),
     downloadsLast30Days: 0,
     downloadsRatio: 0,
     humanDownloadsLast30Days: numeral(0).format('0.[0]a'),
     popular: false,
-    version: formatted.version,
-    description: formatted.description,
-    author: addAvatar(formatted.author),
-    githubRepo: getGitHubRepoInfo(formatted.repository),
-    homepage: getHomePage(formatted.homepage, formatted.repository),
-    license: formatted.license,
-    keywords: formatted.keywords,
-    created: formatted.created,
-    modified: formatted.modified,
-    lastPublisher: addAvatar(formatted.lastPublisher),
-    owners: addAvatars(formatted.owners),
+    version: escape(cleaned.version) || '0.0.0',
+    description: escape(cleaned.description) || 'No description in package.',
+    originalAuthor: cleaned.author,
+    githubRepo,
+    owner,
+    homepage: getHomePage(cleaned.homepage, cleaned.repository),
+    license: escape(cleaned.license) || 'No license',
+    keywords: keywords.map(keyword => escape(keyword)),
+    created: Date.parse(cleaned.created),
+    modified: Date.parse(cleaned.modified),
+    lastPublisher,
+    owners: (cleaned.owners || []).map(formatUser),
   };
 }
 
-function addAvatars(arrayObjs) {
-  if (Array.isArray(arrayObjs) && arrayObjs.length > 0) {
-    return arrayObjs.map(addAvatar);
-  }
-
-  return arrayObjs;
-}
-
-function addAvatar(obj) {
-  if (obj) {
-    let avatar;
-    if (obj.email && obj.email.indexOf('@') !== -1) {
-      avatar = gravatarUrl(obj.email, {size: 180});
-    } else {
-      avatar = 'https://www.gravatar.com/avatar/?size=180';
-    }
-
+function getOwner(githubRepo, lastPublisher) {
+  if (githubRepo) {
     return {
-      ...obj,
-      avatar,
+      name: githubRepo.user,
+      avatar: `http://github.com/${githubRepo.user}.png`,
+      link: `http://github.com/${githubRepo.user}`,
     };
   }
 
-  return obj;
+  return lastPublisher;
+}
+
+function getGravatar(obj) {
+  if (!obj.email || typeof obj.email !== 'string' || obj.email.indexOf('@') !== -1) {
+    return defaultGravatar;
+  }
+
+  return gravatarUrl(obj.email, {size: 180});
 }
 
 function getGitHubRepoInfo(repository) {
@@ -71,8 +78,8 @@ function getGitHubRepoInfo(repository) {
   }
 
   return {
-    user: result[1],
-    project: result[2],
+    user: escape(result[1]),
+    project: escape(result[2]),
   };
 }
 
@@ -82,8 +89,16 @@ function getHomePage(homepage, repository) {
       typeof repository !== 'string' || // or repo is not a string
       repository !== homepage // or repo is different than homepage
     )) {
-    return homepage; // then we consider it a valuable homepage
+    return escape(homepage); // then we consider it a valuable homepage
   }
 
   return null;
+}
+
+function formatUser(user) {
+  return {
+    ...user,
+    avatar: getGravatar(user),
+    link: `https://www.npmjs.com/~${encodeURIComponent(user.name)}`,
+  };
 }
