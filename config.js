fab833dbf87fa0476fb72076deb0fea92d08c567
vvo <vincent.voyer@algolia.com>
2016-12-22 01:41:20 +0100

    lower ratio

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const defaultConfig = {
   npmRegistryEndpoint: 'https://replicate.npmjs.com/registry',
   npmDownloadsEndpoint: 'https://api.npmjs.org/downloads',
-  popularDownloadsRatio: 0.02,
+  popularDownloadsRatio: 0.005,
   appId: 'OFCNCOG2CU',
   apiKey: '',
   indexName: 'npm-search',
