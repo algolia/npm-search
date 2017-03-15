64fe50b582fa560c1e4808719887636b7750597b
Haroen Viaene <hello@haroen.me>
2017-03-15 14:23:08 +0100

    chore(deps) use promise-rat-race

--- github.js
+++ github.js
@@ -1,24 +1,5 @@
 import got from 'got';
-
-function properPromiseRace(promises) {
-  if (promises.length < 1) {
-    return Promise.reject("Can't start a race without promises!");
-  }
-
-  // There is no way to know which promise is rejected.
-  // So we map it to a new promise to return the index when it fails
-  const indexPromises = promises.map((p, index) =>
-    p.catch(() => {
-      throw index;
-    }));
-
-  return Promise.race(indexPromises).catch(index => {
-    // The promise has rejected, remove it from the list of promises and just continue the race.
-    const p = promises.splice(index, 1)[0];
-    p.catch(() => {});
-    return properPromiseRace(promises);
-  });
-}
+import race from 'promise-rat-race';
 
 function getChangelog(
   {
@@ -44,7 +25,7 @@ function getChangelog(
     'history',
   ].map(file => [baseGithubURL.replace(/\/$/, ''), file].join('/'));
 
-  return properPromiseRace(files.map(got, {method: 'HEAD'}))
+  return race(files.map(got, {method: 'HEAD'}))
     .then(({url}) => ({changelogFilename: url}))
     .catch(() => ({changelogFilename: undefined}));
 }
