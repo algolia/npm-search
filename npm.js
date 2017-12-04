ba8aac03af9a32d6aae10793a6b144364dc887f7
Haroen Viaene <fingebimus@me.com>
2017-12-04 18:32:00 +0100

    Add scoped downloads (#85)
    * updated schema
    
    * Alter response for scoped packages (#84)
    
    * chore(optionalFilter): rename onlyFilter to filterOnly
    
    This was changed when OFF became OF. As reported by @pixelastic.
    
    I'm not really sure how this will impact the search, but I think it'll be transparent
    
    * feat(changelog): detect /changelog.markdown
    
    this is used by browserify
    
    * chore(deps): update within range
    
    * chore(deps): major (incl eslint
    
    * chore(deps): set up renovate
    
    * refactor(deps): pin dependencies
    
    * chore(deps): update dependency eslint to v4.12.0 (#76)
    
    * chore(deps): update dependency eslint-config-prettier to v2.9.0 (#77)
    
    * chore(deps): update dependency eslint-config-algolia to v13.0.1 (#75)
    
    * chore(deps): update dependency eslint to v4.12.1 (#81)
    
    * chore(deps): update dependency lint-staged to v6.0.0 (#82)
    
    * updated schema
    
    * add .env handling
    
    * catch removed packages cleanly

--- npm.js
+++ npm.js
@@ -14,6 +14,15 @@ export function info() {
   }));
 }
 
+const suppressError = (e, packages, returnValue) => {
+  log.warn(
+    `Something went wrong asking the downloads for \n${Array.from(
+      packages
+    ).join(',')} \n${e}`
+  );
+  return returnValue;
+};
+
 export function getDownloads(pkgs) {
   // npm has a weird API to get downloads via GET params, so we split pkgs into chunks
   // and do multiple requests to avoid weird cases when concurrency is high
@@ -21,6 +30,11 @@ export function getDownloads(pkgs) {
     .map(pkg => pkg.name)
     .filter(name => name[0] !== '@' /* downloads for scoped packages fails */)
     .map(name => encodeURIComponent(name));
+  const encodedScopedPackageNames = pkgs
+    .map(pkg => pkg.name)
+    .filter(name => name[0] === '@')
+    .map(name => encodeURIComponent(name));
+
   // why do we do this? see https://github.com/npm/registry/issues/104
   encodedPackageNames.unshift('');
   const pkgsNamesChunks = chunk(encodedPackageNames, 100).map(names =>
@@ -33,25 +47,14 @@ export function getDownloads(pkgs) {
     ...pkgsNamesChunks.map(pkgsNames =>
       got(`${c.npmDownloadsEndpoint}/point/last-month/${pkgsNames}`, {
         json: true,
-      }).catch(e => {
-        log.warn(
-          `Something went wrong asking the downloads for \n${Array.from(
-            pkgsNames
-          ).join(',')} \n${e}`
-        );
-        return {
-          body: Array.from(pkgsNames).reduce(
-            (acc, current) => ({
-              ...acc,
-              [current]: {
-                downloads: 0,
-                package: current,
-              },
-            }),
-            {}
-          ),
-        };
+      }).catch(e => suppressError(e, pkgsNames, { body: {} }))
+    ),
+    ...encodedScopedPackageNames.map(pkg =>
+      got(`${c.npmDownloadsEndpoint}/point/last-month/${pkg}`, {
+        json: true,
       })
+        .then(res => ({ body: { [res.body.package]: res.body } }))
+        .catch(e => suppressError(e, [pkg], { body: {} }))
     ),
   ]).then(
     (
@@ -116,6 +119,12 @@ export function getDependents(pkgs) {
           dependents: value,
           humanDependents: numeral(value).format('0.[0]a'),
         }))
+        .catch(e =>
+          suppressError(e, [name], {
+            dependents: 0,
+            humanDependents: '0',
+          })
+        )
     )
   );
 }
