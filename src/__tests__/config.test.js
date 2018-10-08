e83bb6595a4562cab3967d35cfe6267ea3eb339d
Haroen Viaene <hello@haroen.me>
2018-10-08 17:16:49 +0200

    test(config): make sure to fail when objectID isn't allowed

--- src/__tests__/config.test.js
+++ src/__tests__/config.test.js
@@ -7,3 +7,17 @@ it('gets the correct keys from env variables', () => {
   expect(config.maxObjSize).toBe(450000);
   expect(config).toMatchSnapshot();
 });
+
+const objectIDRe = /^[A-Za-z0-9_-]+$/;
+
+it('sets correct objectIDs for query rules', () => {
+  config.indexRules.forEach(({ objectID }) => {
+    expect(objectID).toMatch(objectIDRe);
+  });
+});
+
+it('sets correct objectIDs for synonyms', () => {
+  config.indexSynonyms.forEach(({ objectID }) => {
+    expect(objectID).toMatch(objectIDRe);
+  });
+});
