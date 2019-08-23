13a8971c1661164d3f29709cd6be3b998c867157
Samuel Bodin <samuel.bodin@algolia.com>
2019-08-23 18:13:03 +0200

    dos

--- src/config.js
+++ src/config.js
@@ -22,6 +22,7 @@ const defaultConfig = {
   bootstrapIndexName: 'npm-search-bootstrap',
   replicateConcurrency: 10,
   bootstrapConcurrency: 100,
+  packagesPrefetchMax: 200,
   timeToRedoBootstrap: ms('2 weeks'),
   seq: null,
   indexSettings: {
