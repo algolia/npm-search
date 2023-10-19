60c8079bebcfff1065f8b64f71497e7b27a2aeb0
Tim Carry <tim@pixelastic.com>
2023-10-19 17:31:51 +0200

    feat(ci): Publish Docker image to GitHub Packages from CI

--- release.config.js
+++ release.config.js
@@ -1,13 +1,57 @@
 /* eslint-disable import/no-commonjs */
 /* eslint-disable no-template-curly-in-string */
+/**
+ * We use semantic-release to automate the publishing of new versions based on
+ * the commit history: whenever a commit is pushed to the master branch, it
+ * checks if any commit had a BREAKING CHANGE / feat() / fix() message, and
+ * publishes (or not) a new major.minor/patch version accordingly.
+ *
+ * See: https://github.com/semantic-release/semantic-release.
+ *
+ * Semantic-release executes steps in order (from verifyConditions to
+ * success/fail). For each step, it execute the matching code in each plugin (if
+ * such exists). If any step fails, the whole process stop.
+ *
+ * As we are using a mix of core and community plugins, as well as slightly
+ * diverging from the default use-case, we explictly define the order of plugins
+ * in each step instead of relying on the default order.
+ *
+ * The current configuration will:
+ * - Check if a new version needs to be published (and stop if not)
+ * - Update the version number in package.json accordingly
+ * - Update the CHANGELOG.md with the changes
+ * - Create a new commit, and tag it with the version number
+ * - Publish the code source to GitHub Releases (not very useful).
+ *
+ * Specifically, it does not:
+ * - Publish the code to npm (this is not an npm module)
+ * - Publish the Docker image (yarn publish:docker takes care of that).
+ **/
 module.exports = {
   branches: 'master',
-  verifyConditions: ['@semantic-release/github'],
+  plugins: [
+    // Those 4 plugins are part of the core of semantic-release
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    // Those 2 are additional plugins
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+  ],
+  // Below are the various steps
+  // Source: https://semantic-release.gitbook.io/semantic-release/usage/plugins
+  // We explicitly define because it allows us to:
+  // - remove steps that we don't need (for example verifying npm credentials as
+  //   we don't publish on npm)
+  // - put steps in order (for example updating the changelog file before
+  //   committing it)
+  verifyConditions: ['@semantic-release/github', '@semantic-release/git'],
+  analyzeCommits: ['@semantic-release/commit-analyzer'],
+  verifyRelease: [],
+  generateNotes: ['@semantic-release/release-notes-generator'],
   prepare: [
-    {
-      path: '@semantic-release/changelog',
-      changelogFile: 'CHANGELOG.md',
-    },
+    '@semantic-release/changelog',
     '@semantic-release/npm',
     {
       path: '@semantic-release/git',
@@ -17,7 +61,7 @@ module.exports = {
     },
   ],
   publish: ['@semantic-release/github'],
+  addChannel: [],
   success: [],
   fail: [],
-  npmPublish: false,
 };
---
+++ scripts/publish-check.js
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-commonjs */
+/* eslint-disable no-console */
+/* eslint-disable no-process-exit */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const semanticRelease = require('semantic-release');
+const { WritableStreamBuffer } = require('stream-buffers');
+
+(async () => {
+  console.log('Analyzing commits since last version...');
+  // Execute semantic-release with only the commit-analyzer step, to see if
+  // a new release is needed
+  const { nextRelease } = await semanticRelease(
+    {
+      dryRun: true,
+      plugins: ['@semantic-release/commit-analyzer'],
+      verifyConditions: [],
+      analyzeCommits: ['@semantic-release/commit-analyzer'],
+      verifyRelease: [],
+      generateNotes: [],
+      prepare: [],
+      publish: [],
+      addChannel: [],
+      success: [],
+      fail: [],
+    },
+    // Redirect output to new streams, to make the script silent
+    {
+      stdout: new WritableStreamBuffer(),
+      stderr: new WritableStreamBuffer(),
+    }
+  );
+
+  // Exit with 0 if a new version must be released, 1 if nothing to do
+  if (nextRelease?.version) {
+    console.log(
+      `Commits analyzed warrant a release of version ${nextRelease.version}`
+    );
+    process.exit(0);
+  }
+  console.log('No new version to publish');
+  process.exit(1);
+})();
