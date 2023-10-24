ddcb9142ff5da4c75a94a407a4e3320fc93b5290
Tim Carry <tim@pixelastic.com>
2023-10-24 09:49:18 +0200

    Moving failing tests into their own directory

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-  name: 'npm',
-
   transform: {
     '^.+\\.[jt]sx?$': 'ts-jest',
   },
   testMatch: ['<rootDir>/src/**/*.test.[jt]s'],
+  // By default, ignore the slow and flaky tests testing external APIs. Those
+  // will be run specifically with `yarn run test:sanity-check`
+  testPathIgnorePatterns: ['sanity-check'],
   globals: {
     'ts-jest': {
       diagnostics: false,
