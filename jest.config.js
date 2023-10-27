e3e08a97fd943c973a167cb971d88c80ad15e2c7
Tim Carry <tim@pixelastic.com>
2023-10-27 13:20:32 +0200

    chore: fix build

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,22 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
   transform: {
-    '^.+\\.[jt]sx?$': 'ts-jest',
+    '^.+\\.[jt]sx?$': [
+      'ts-jest',
+      {
+        diagnostics: false,
+        tsconfig: `tsconfig.json`,
+      },
+    ],
   },
   testMatch: ['<rootDir>/src/**/*.test.[jt]s'],
   // By default, ignore the slow and flaky tests testing external APIs. Those
   // will be run specifically with `yarn run test:api-control`
   testPathIgnorePatterns: ['api-control'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      diagnostics: false,
-      tsconfig: `tsconfig.json`,
-    }]
-  },
   testEnvironment: 'node',
   modulePaths: ['src'],
   snapshotFormat: {
-   escapeString: true,
-   printBasicPrototype: true
-  }
+    escapeString: true,
+    printBasicPrototype: true,
+  },
 };
